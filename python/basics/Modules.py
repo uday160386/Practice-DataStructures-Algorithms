006d30f584b8dda65a082ae19b1b6b5af3386f97
uday160386 <uday160386@gmail.com>
2021-03-19 07:33:09 +0800

    clean up

---
+++ python/basics/Modules.py
@@ -0,0 +1,9 @@
+from basics import IterationStatements
+from basics.TestClass import Student
+
+print(IterationStatements.power_of_number(10, 20))
+
+student1 = Student("Uday", "21")
+
+print(student1.get_name())
+print(student1.get_age())
---
+++ python/basics/README.md
@@ -0,0 +1,2 @@
+# Coding
+This repository is created to maintain my code samples using Python and Data structures.
